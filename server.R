3d472310733bb9439ed38fc103daffc95a2f90f4
nitokushite <nitokushite11@gmail.com>
2021-02-09 13:18:18 +0300

    creating KPIS and the last one failed

--- server.R
+++ server.R
@@ -6,7 +6,10 @@ shinyServer(function(input, output) {
     output$histogram<-renderPlot({
         hist(faithful$eruptions,breaks = input$bins)
     })
-    
+    output$approvedSales <- renderInfoBox({
+        infoBox("Approval Sales","10,00,000",
+                icon = icon("bar-chart-o"))
+    })
 })
 
 
