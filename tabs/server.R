0ee9dacb0ce0b54e1961f8af93eb116eeb3e05e2
nitokushite <nitokushite11@gmail.com>
2021-02-10 05:09:45 +0300

    creating reactive function

--- tabs/server.R
+++ tabs/server.R
@@ -3,14 +3,19 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
+    mtreact <-reactive({
+        mtcars[,c("mpg",input$ngear)]
+    })
+    
     output$mtcars<- renderTable({
-        mtcars[,c("mpg", input$ngear)]
+        mtreact()
     })
     output$sum<- renderPrint({
-        summary(mtcars[,c("mpg",input$ngear)])
+       summary(mtreact()) 
     })
     output$plot<-renderPlot({
-        with(mtcars[,c("mpg",input$ngear)],boxplot(mpg~gear))
+        with(mtreact(),boxplot(mpg~mtreact()[,2]))
     })
 
 })
